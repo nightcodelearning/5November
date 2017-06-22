00bdef1fd0eaa7ea3aca68592c8e135a97aa8e37
night code learning <nightcodelearning@gmail.com>
2017-06-22 13:29:09 -0500

    Initial Commit

---
+++ PlatziSimpleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlatziSimpleApp
+//
+//  Created by Inés Rojas on 6/22/17.
+//  Copyright © 2017 Inés Rojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
